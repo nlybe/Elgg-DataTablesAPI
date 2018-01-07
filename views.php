6bfc658222868cbb2144383dd712390f6cec6e6f
nlybe <nikos@lyberakis.gr>
2018-01-07 12:36:55 +0200

    updated path on views

--- views.php
+++ views.php
@@ -7,7 +7,7 @@
 return [
     'default' => [
         'datatables_api/images/' => __DIR__ . '/vendor/datatables/datatables/media/images',
-        'datatables.js' => __DIR__ . '/vendor/datatables/datatables/media/js/jquery.dataTables.js',
-        'datatables.css' => __DIR__ . '/vendor/datatables/datatables/media/css/jquery.dataTables.css',
+        'datatables.js' => elgg_get_site_url() . 'vendor/datatables/datatables/media/js/jquery.dataTables.js',
+        'datatables.css' => elgg_get_site_url() . 'vendor/datatables/datatables/media/css/jquery.dataTables.css',
     ],
 ];
