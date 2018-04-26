0172b5d21aac8e53a32b042814a547639a913eaa
nlybe <nikos@lyberakis.gr>
2018-04-26 08:19:35 +0300

    added length menu in order to select the number of records to display

--- views/default/datatables_api/dtapi_ajax.js
+++ views/default/datatables_api/dtapi_ajax.js
@@ -23,14 +23,13 @@ define(function (require) {
         
     }
     
-    dtOptions['lengthMenu'] = [ [3, 10, 25, 50, -1], [3, 10, 25, 50, "All"] ];
+    dtOptions['lengthMenu'] = [ [10, 25, 50, -1], [10, 25, 50, "All"] ];
 
     dtOptions['ajax'] = $(".dt_hidden").data("ajaxsource");
     dtOptions['pageLength'] = $(".dt_hidden").data("limit");
     dtOptions['processing'] = true;
     dtOptions['serverSide'] = true;
     
-console.log(dtOptions['pageLength']);    
     var titles =  $(".dt_hidden").data("titles");
     var titles_arr = titles.split(",");
     var list = []; 
